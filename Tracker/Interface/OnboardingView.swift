60db650ca407b8b3d1c93d6eaa2ec6532ef572f8
Anton Filipchuk <fantom.qm@gmail.com>
2024-05-04 12:20:47 +0300

    Добавлен экран онбординга с использованием UIPageViewController и UIPageControl

---
+++ Tracker/Interface/OnboardingView.swift
@@ -0,0 +1,92 @@
+//
+//  OnboardingView.swift
+//  Tracker
+//
+//  Created by Anton Filipchuk on 03.05.2024.
+//
+
+import UIKit
+
+final class OnboardingView: UIViewController {
+    
+    private lazy var onboardingImage: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleToFill
+        return image
+    }()
+    
+    private lazy var onboardingLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .yBlack
+        label.font = .systemFont(ofSize: 32, weight: .bold)
+        return label
+    }()
+    
+    private lazy var onboardingButton = BlackButton(title: "Вот это технологии!")
+    
+    init(_ image: String,_ text: String) {
+        super.init(nibName: nil, bundle: nil)
+        setupOnboarding(image, text)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension OnboardingView {
+    func setupOnboarding(_ image: String, _ text: String) {
+        addImage(image)
+        addLabel(text)
+        addSubView()
+        addLayout()
+        addTarget()
+    }
+    
+    func addLabel(_ text: String) {
+        onboardingLabel.text = text
+        onboardingLabel.numberOfLines = 2
+        onboardingLabel.textAlignment = .center
+    }
+    
+    func addImage(_ image: String) {
+        onboardingImage.image = UIImage(named: image)
+    }
+    
+    func addSubView() {
+        view.insertSubview(onboardingImage, at: 0)
+        view.addSubview(onboardingLabel)
+        view.addSubview(onboardingButton)
+    }
+    
+    func addLayout() {
+        onboardingLabel.translatesAutoresizingMaskIntoConstraints = false
+        onboardingImage.translatesAutoresizingMaskIntoConstraints = false
+        onboardingButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            onboardingImage.topAnchor.constraint(equalTo: view.topAnchor),
+            onboardingImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            onboardingImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            onboardingImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            onboardingLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 455),
+            onboardingLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            onboardingLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            onboardingButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            onboardingButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            onboardingButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        ])
+    }
+    
+    func addTarget() {
+        onboardingButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    @objc func didTapButton() {
+        let viewController = TabBarController()
+        viewController.modalPresentationStyle = .fullScreen
+        present(viewController, animated: true)
+    }
+}
---
+++ Tracker/ViewControllers/OnboardingViewController.swift
@@ -0,0 +1,77 @@
+//
+//  OnboardingViewController.swift
+//  Tracker
+//
+//  Created by Anton Filipchuk on 03.05.2024.
+//
+
+import UIKit
+
+final class OnboardingViewController: UIPageViewController {
+    private lazy var pages: [UIViewController] = {
+        let blue = OnboardingView("blue", "Отслеживайте только \n то, что хотите")
+        let red = OnboardingView("red", "Даже если это \n не литры воды и йога")
+        return [blue, red]
+    }()
+    
+    private lazy var pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.numberOfPages = pages.count
+        pc.currentPage = 0
+        pc.currentPageIndicatorTintColor = .yBlack
+        pc.pageIndicatorTintColor = .yBlack.withAlphaComponent(0.3)
+        pc.translatesAutoresizingMaskIntoConstraints = false
+        return pc
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        addPageControl()
+        dataSource = self
+        delegate = self
+    }
+}
+
+private extension OnboardingViewController {
+    func setup() {
+        if let first = pages.first {
+            setViewControllers([first], direction: .forward, animated: false)
+        }
+    }
+    
+    func addPageControl() {
+        view.addSubview(pageControl)
+        
+        NSLayoutConstraint.activate([
+            pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pageControl.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -168)
+        ])
+    }
+    
+}
+
+extension OnboardingViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+        let previousIndex = viewControllerIndex - 1
+        guard previousIndex >= 0 else { return nil }
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+        let nextIndex = viewControllerIndex + 1
+        guard nextIndex < pages.count else { return nil }
+        return pages[nextIndex]
+    }
+}
+
+extension OnboardingViewController: UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if let currentViewController = pageViewController.viewControllers?.first,
+           let currentIndex = pages.firstIndex(of: currentViewController) {
+            pageControl.currentPage = currentIndex
+        }
+    }
+}
